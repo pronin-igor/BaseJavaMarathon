ae1578e854750bb8ca52f78d362383ec7c726e22
pronin-igor <gosha.pronin@gmail.com>
2023-12-12 17:49:46 +0300

    init

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class Car {
+    private String model;
+    private String color;
+    private int issueYear;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setIssueYear(int issueYear) {
+        this.issueYear = issueYear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getIssueYear() {
+        return issueYear;
+    }
+}
---
+++ src/main/java/day6/Task1.java
@@ -0,0 +1,9 @@
+package day6;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Car car = new Car("mazda", "white", 2018);
+        // car.info();
+        // System.out.println(car.yearDifference(2023));
+    }
+}
