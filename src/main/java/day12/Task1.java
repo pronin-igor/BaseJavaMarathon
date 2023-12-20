556639a93bb5720d37fa348993b5a073c57158fc
pronin-igor <gosha.pronin@gmail.com>
2023-12-20 17:54:18 +0300

    *

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,19 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Audi");
+        list.add("BMW");
+        list.add("Lada");
+        list.add("Tesla");
+        list.add("Mercedes");
 
+        System.out.println(list);
+        list.add(2, "Audi");
+        System.out.println(list);
     }
 }
