eaf18f10bc7009dc04a2bc191aa606abae6aa13c
pronin-igor <gosha.pronin@gmail.com>
2023-12-22 16:33:35 +0300

    day 14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,35 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String path = "day14.txt";
+        File file = new File(path);
+    }
 
+    public static void printSumDigits(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String numbersString[] = scanner.nextLine().split(" ");
+
+            if (numbersString.length != 10) {
+                System.out.println("Некорректный входной файл");
+                return;
+            }
+
+            int sum = 0;
+            for (String numberString : numbersString) {
+                sum += Integer.parseInt(numberString);
+            }
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Файл не найден");
+        }
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,47 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.management.BadAttributeValueExpException;
+
 public class Task3 {
     public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
 
+        String path = "people.txt";
+        File file = new File(path);
+        list = parseFileToObjList(file);
+        System.out.println(list);
+    }
+
+    public static List<Person> parseFileToObjList(File file) {
+        List<Person> list = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                String personString[] = string.split(" ");
+
+                if (Integer.parseInt(personString[1]) < 0) {
+                    throw new BadAttributeValueExpException(personString);
+                }
+
+                list.add(new Person(personString[0], Integer.parseInt(personString[1])));
+            }
+        } catch (BadAttributeValueExpException e) {
+            System.out.println("Некорректный входной файл");
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        
+        return list;
     }
 }
