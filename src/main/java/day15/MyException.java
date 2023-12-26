3de68255e7e070ba3d9ccd1a7f529e7b7a85126f
pronin-igor <gosha.pronin@gmail.com>
2023-12-26 15:54:58 +0300

    day 15

---
+++ src/main/java/day15/MyException.java
@@ -0,0 +1,7 @@
+package day15;
+
+public class MyException extends Exception{
+    public MyException(String exception) {
+        super(exception);
+    }
+}
--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,37 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String path = "src/main/resources/shoes.csv";
+        File file = new File(path);
+        parseCsvToTxt(file);
+    }
 
+    public static void parseCsvToTxt(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            PrintWriter pw = new PrintWriter("src/main/resources/missing_shoes.txt");
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String splitLine[] = line.split(";");
+                if (splitLine.length != 3) {
+                    throw new MyException("Некорректный файл");
+                }
+                if (splitLine[2].equals("0")) {
+                    pw.println(String.join(",", splitLine)); 
+                }
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
     }
 }
