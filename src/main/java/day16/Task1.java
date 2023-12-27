ae75ed0fae98f41438f31ae6afbcbaa7474f1b46
pronin-igor <gosha.pronin@gmail.com>
2023-12-27 12:12:02 +0300

    day 16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,31 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        String path = "day14.txt";
+        File file = new File(path);
+        printResult(file);
     }
-}
 
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String lineArray[] = line.split(" ");
+            double sum = 0;
+            for (String number: lineArray) {
+                sum += Double.parseDouble(number);
+            }
+            double average = sum / (double) lineArray.length;
+            System.out.print(average);
+            System.out.printf(" --> %.3f", average, average);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}   
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,52 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        try {
+            PrintWriter pw1 = new PrintWriter("file1.txt");
+            PrintWriter pw2 = new PrintWriter("file2.txt");
+            
+            int randomNumber = 0;
+            double groupSum = 0;
+            for (int i = 0; i < 1000; i++) {
+                randomNumber = (int) (Math.random() * 100);
+                pw1.print(randomNumber + " ");
 
+                groupSum += randomNumber;
+                if (i % 20 == 0 && i !=0) {
+                    pw2.print(groupSum / 20 + " ");
+                    groupSum = 0;
+                }
+            }
+            pw1.close();
+            pw2.close();
+
+
+            printResult(new File("file2.txt"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String lineArray[] = line.split(" ");
+            double lineArraySum = 0;
+            for (String element: lineArray) {
+                lineArraySum += Double.parseDouble(element);
+            }
+            System.out.println((int) lineArraySum);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
